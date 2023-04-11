976d9d0a535b00967d022e4cbafacd574d45c310
EmilyErickson <emilyerickson2002@gmail.com>
2023-04-10 19:33:39 -0500

    Initial file set up

---
+++ script.js
@@ -0,0 +1,22 @@
+//Get access to all of the form sections and inputs
+//email
+//country
+//zip code
+//password
+//password confirmation
+
+//email onchange listener
+// run function to validate input and create custom
+//validtion message
+
+// country section onchange listener
+//run function to verify zip code and county match. if they
+//don't match, run another function with custom validation
+
+//password section onchange listener
+//run function to verify passwords match. if not, run function
+//to create custom validation message
+
+//submit button submit listener
+//run all functions to verify accuracy of form inputs
+//create congratulation message to user
